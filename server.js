9d78e00718adf4088068e8b531a1df62a2bb646c
ichmel <abangoichmel@gmail.com>
2023-12-06 00:37:33 +0100

    get user details

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.get('/api/get', (req,res) => {
 })
 
 
+app.get('/api/get_user_details',(req,res) => {
+    res.sed({
+        user:{
+            name:'stam',
+            age:22,
+            contact: 1234
+        }
+    })
+})
+
 app.listen(8080,()=> {
     console.log("lintening to 8080");
 })
